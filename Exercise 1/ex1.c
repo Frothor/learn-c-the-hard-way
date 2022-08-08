cfd839bbec059e62a49586a52d9b30ea35454b1a
frothor <pmjakubiak13@gmail.com>
2022-08-08 23:00:28 +0200

    Added 3 first exercises. Gitignore adjusted.

---
+++ Exercise 1/ex1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/* This is coment. */
+int main(int argc, char *argv[])
+{
+	int distance = 100;
+
+	// this is also a comment
+	printf("You are %s miles away.\n", distance);
+
+	return 0;
+}
